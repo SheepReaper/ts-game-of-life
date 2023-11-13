7e6646a017775a130b6698c7c311f19d7981641a
Bryan Gonzalez <bgonza868@gmail.com>
2023-11-13 14:43:18 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import p5 from 'p5';
+
+const sketch = (p:p5) => {
+  p.setup = () => {
+    // Runs once
+    p.createCanvas(800, 800);
+    p.background(200);
+  }
+  p.draw = () => {
+    // Runs every frame
+  }
+};
+
+const element = document.getElementById("canvas-frame");
+
+if (element) {
+  new p5(sketch, element);
+}
